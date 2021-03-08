1208fd49e3985664a7f8d26bed909a9586e7be5e
gbudau <54190400+gbudau@users.noreply.github.com>
2021-03-08 23:43:21 +0100

    Add ex00
    Added ex00

--- ex00/Fixed.class.cpp
+++ ex00/Fixed.class.cpp
@@ -1,1 +1,37 @@
 #include "Fixed.class.hpp"
+
+Fixed::Fixed() : _value(0) {
+	std::cout << "Default constructor called" << std::endl;
+	return;
+}
+
+Fixed::Fixed(Fixed const & src) {
+	std::cout << "Copy constructor called" << std::endl;
+	*this = src;
+	return;
+}
+
+Fixed::~Fixed() {
+	std::cout << "Destructor called" << std::endl;
+	return;
+}
+
+int			Fixed::getRawBits() const {
+	std::cout << "getRawBits member function called" << std::endl;
+	return this->_value;
+}
+
+void		Fixed::setRawBits(int const raw) {
+	std::cout << "setRawBits member function called" << std::endl;
+	this->_value = raw;
+}
+
+Fixed &		Fixed::operator=(Fixed const & rhs) {
+	std::cout << "Assignation operator called" << std::endl;
+	if (this != &rhs) {
+		this->_value = rhs.getRawBits();
+	}
+	return *this;
+}
+
+const int	Fixed::_fractional_bits = 0;
--- ex00/Fixed.class.hpp
+++ ex00/Fixed.class.hpp
@@ -1,4 +1,19 @@
 #ifndef FIXED_CLASS_HPP_
 #define FIXED_CLASS_HPP_
+#include <cstdint>
+#include <iostream>
+
+class	Fixed {
+	public:
+		Fixed();
+		Fixed(Fixed const & src);
+		~Fixed();
+		int					getRawBits() const;
+		void				setRawBits(int const raw);
+		Fixed &				operator=(Fixed const & rhs);
+	private:
+		int					_value;
+		static const int	_fractional_bits;
+};
 
 #endif  // FIXED_CLASS_HPP_
